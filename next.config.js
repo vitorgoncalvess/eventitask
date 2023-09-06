c16a745588d5d320ecfa784373aadd782df244f6
vitorgoncalvess <vitor.gsilva@sptech.school>
2023-09-06 12:01:01 -0300

    att

--- next.config.js
+++ next.config.js
@@ -1,7 +1,14 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   images: {
-    domains: ["avatars.githubusercontent.com", "i.kym-cdn.com"],
+    remotePatterns: [
+      {
+        protocol: "https",
+        hostname: "**",
+        port: "",
+        pathname: "**",
+      },
+    ],
   },
   typescript: {
     ignoreBuildErrors: true,

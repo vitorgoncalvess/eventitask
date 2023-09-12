66d6eaf05891975edf67dcf13642030d8208cb3c
vitorgoncalvess <vitor.gsilva@sptech.school>
2023-09-12 12:38:31 -0300

    att

--- app/api/boards/route.ts
+++ app/api/boards/route.ts
@@ -2,10 +2,6 @@ import { NextResponse } from "next/server";
 import { query } from "@/app/(lib)/db";
 
 export async function GET(req: Request) {
-  const value = req.headers.get("authorization")?.replace("Bearer ", "");
-  if (!value) {
-    return NextResponse.json({ message: "Token inexistente" }, { status: 400 });
-  }
   try {
     const boards = await query("SELECT * FROM board");
 

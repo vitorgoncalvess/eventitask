f77243852ecb33abb58574d83b5683938dd8cb0c
vitorgoncalvess <vitor.gsilva@sptech.school>
2023-09-12 12:25:30 -0300

    att

--- app/(lib)/db.js
+++ app/(lib)/db.js
@@ -1,22 +1,22 @@
-import mysql from 'mysql2';
+import mysql from "mysql2";
 
 const aws = {
-  host: '52.45.208.66',
-  port: '3306',
-  database: 'eventitask',
-  user: 'root',
-  password: '1991',
+  host: "52.45.208.66",
+  port: "3306",
+  database: "eventitask",
+  user: "root",
+  password: "1991",
 };
 
 const local = {
-  host: 'localhost',
-  port: '3306',
-  database: 'eventitask',
-  user: 'root',
-  password: '1991',
+  host: "localhost",
+  port: "3306",
+  database: "eventitask",
+  user: "root",
+  password: "1991",
 };
 
-const db = mysql.createPool(local);
+const db = mysql.createPool(aws);
 
 export const query = (sql, params) => {
   return new Promise((resolve, reject) => {

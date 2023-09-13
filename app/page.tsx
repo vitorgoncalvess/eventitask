e19b60b0d6d0d2e6592b7bd1356099193b8b7217
vitorgoncalvess <vitor.gsilva@sptech.school>
2023-09-12 22:25:58 -0300

    att

--- app/page.tsx
+++ app/page.tsx
@@ -1,36 +1,36 @@
-"use client";
+'use client';
 
-import Input from "./_components/Input";
-import { Button } from "@nextui-org/react";
-import { FormEvent, useState } from "react";
-import axiosInstance from "./_axios/config";
-import { useRouter } from "next/navigation";
-import Logo from "./_components/Logo";
-import Loading from "./_components/Loading";
+import Input from './_components/Input';
+import { Button } from '@nextui-org/react';
+import { FormEvent, useState } from 'react';
+import axiosInstance from './_axios/config';
+import { useRouter } from 'next/navigation';
+import Logo from './_components/Logo';
+import Loading from './_components/Loading';
 
 export default function Home() {
-  const [email, setEmail] = useState("");
-  const [senha, setSenha] = useState("");
+  const [email, setEmail] = useState('');
+  const [senha, setSenha] = useState('');
   const [loading, setLoading] = useState(false);
 
   const router = useRouter();
 
   function handleSubmit(e: any) {
-    if (e.key && e.key != "Enter") {
+    if (e.key && e.key != 'Enter') {
       return;
     }
     e.preventDefault();
     setLoading(true);
     axiosInstance
-      .post("/usuarios", { email, senha })
+      .post('/usuarios', { email, senha })
       .then((response) => {
         if (response.status === 200) {
-          localStorage.setItem("token", response.data.token);
-          localStorage.setItem("id", response.data.id);
-          axiosInstance.get("/boards").then((response: any) => {
+          localStorage.setItem('token', response.data.token);
+          localStorage.setItem('id', response.data.id);
+          axiosInstance.get('/boards').then((response: any) => {
             const { boards } = response.data;
             if (boards.length === 0) {
-              router.push("/home");
+              router.push('/home');
             } else {
               router.push(`/home/${boards[0].id}`);
             }
@@ -49,7 +49,7 @@ export default function Home() {
         <div className="flex items-center absolute left-4 top-4 select-none">
           <Logo size="base" />
         </div>
-        <h1 className="text-2xl">Login</h1>
+        <h1 className="text-2xl">Foi</h1>
         <form
           onKeyDown={(e) => handleSubmit(e)}
           className="flex flex-col gap-8 w-full"

39414170cd9fb3525b88874793f54849c21728b6
vitorgoncalvess <vitor.gsilva@sptech.school>
2023-09-19 20:24:30 -0300

    att

--- app/_components/Sections.tsx
+++ app/_components/Sections.tsx
@@ -1,7 +1,7 @@
-"use client";
+'use client';
 
-import React from "react";
-import { Dash } from "../_utils/interfaces";
+import React from 'react';
+import { Dash } from '../_utils/interfaces';
 import {
   Card,
   CardBody,
@@ -9,32 +9,32 @@ import {
   Chip,
   CircularProgress,
   ScrollShadow,
-} from "@nextui-org/react";
-import colors from "../_utils/colors";
+} from '@nextui-org/react';
+import colors from '../_utils/colors';
 
 const Sections = ({ sections }: any) => {
   return (
     <ScrollShadow
       orientation="horizontal"
-      className="flex items-center gap-2 p-2 relative"
+      className="flex items-center gap-2 p-2 relative h-full"
       size={0}
     >
-      <div className="self-end flex items-center gap-4">
+      <div className="flex items-center h-full p-4 gap-4">
         {sections.length &&
           sections?.map((sec) => (
             <Card
               isPressable
               classNames={{
-                base: `${sec.color} text-white w-[175px]`,
+                base: `${sec.color} text-white h-full w-[15vw]`,
               }}
               key={sec.id}
             >
               <CardBody className="flex items-center justify-center pb-1">
                 <CircularProgress
                   classNames={{
-                    svg: "w-20 h-20",
-                    indicator: "stroke-[rgb(255,255,255,0.7)]",
-                    value: "text-lg font-medium",
+                    svg: 'w-20 h-20',
+                    indicator: 'stroke-[rgb(255,255,255,0.7)]',
+                    value: 'text-lg font-medium',
                   }}
                   showValueLabel
                   value={sec.completion_percentage}
@@ -43,7 +43,7 @@ const Sections = ({ sections }: any) => {
               <CardFooter className="flex items-center justify-center pt-0">
                 <Chip
                   classNames={{
-                    base: "text-white border-white",
+                    base: 'text-white border-white',
                   }}
                   variant="bordered"
                 >
--- app/api/usuarios/route.ts
+++ app/api/usuarios/route.ts
@@ -1,13 +1,13 @@
-import bcrypt from "bcrypt";
-import jwt from "jsonwebtoken";
-import { NextResponse } from "next/server";
-import { query } from "@/app/_lib/db";
+import bcrypt from 'bcrypt';
+import jwt from 'jsonwebtoken';
+import { NextResponse } from 'next/server';
+import { query } from '@/app/_lib/db';
 
-const key = process.env.SECRET_KEY || "chave-secreta";
+const key = process.env.SECRET_KEY || 'chave-secreta';
 
 export async function GET() {
   try {
-    const users = await query("SELECT id, name, img FROM usuario");
+    const users = await query('SELECT id, name, img FROM user');
     return NextResponse.json(users);
   } catch (err) {
     return NextResponse.json(err);
@@ -17,23 +17,21 @@ export async function GET() {
 export async function POST(req: Request) {
   const { email, senha } = await req.json();
   try {
-    const result = await query("SELECT * FROM usuario WHERE email = ?", [
-      email,
-    ]);
+    const result = await query('SELECT * FROM user WHERE email = ?', [email]);
 
     if (!result[0]) {
       return NextResponse.json(
-        { message: "Usuario inexistente" },
+        { message: 'Usuario inexistente' },
         { status: 404 },
       );
     }
 
     if (await bcrypt.compare(senha, result[0].password)) {
-      const token = jwt.sign(result[0], key, { expiresIn: "1h" });
+      const token = jwt.sign(result[0], key, { expiresIn: '1h' });
       return NextResponse.json({ ...result[0], token });
     } else {
       return NextResponse.json(
-        { message: "E-mail ou senha invalidos" },
+        { message: 'E-mail ou senha invalidos' },
         { status: 400 },
       );
     }
